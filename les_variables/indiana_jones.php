51a6dbe215b95d2cea9b93d71f4aa45b390f6487
MatthiasWanner <matthias.wanner@gmail.com>
2022-02-13 18:49:29 +0100

    :seedling: (php-2) Quest #2 : variables

---
+++ les_variables/indiana_jones.php
@@ -0,0 +1,11 @@
+<?php
+$originalTitle = "Indiana Jones and the Last Crusade";
+$isViewedByMe = true;
+$yearReleased = 1989;
+$imdbRating = 8.2;
+?>
+
+<p>Original title : <?php echo($originalTitle) ?><p>
+<p>I <?php echo($isViewedByMe ? 'have' : "haven't")?> seen this film<p>
+<p>It was released in <?php echo($yearReleased) ?><p>
+<p>It has an IMDB rating of <?php echo($imdbRating) ?>/10<p>
